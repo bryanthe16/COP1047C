65d14e135cb24ba92436d29b9d36ce0a5c71ffef
bryanthe16 <100640170+bryanthe16@users.noreply.github.com>
2022-02-28 17:59:48 -0500

    Update project 1.py

--- project 1.py
+++ project 1.py
@@ -5,8 +5,7 @@ dependent_deduction = 3000.0
 gross_income = float(input("Enter the gross income: "))
 num_dependents = int(input("Enter the number of dependents: "))
 
-taxed_income = gross_income - standard_deduction - \
-                dependent_deduction * num_dependents
+taxed_income = gross_income - standard_deduction - dependent_deduction * num_dependents
 income_tax = taxed_income * tax_rate
 
 print("The income tax is $" + str(income_tax))
