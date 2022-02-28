d97f9b7d56989e172cfe4bdce337f954216dcdb6
bryanthe16 <100640170+bryanthe16@users.noreply.github.com>
2022-02-28 17:56:17 -0500

    Add files via upload

---
+++ project 1.py
@@ -0,0 +1,13 @@
+tax_rate = 0.20
+standard_deduction = 10000.0
+dependent_deduction = 3000.0
+
+gross_income = float(input("Enter the gross income: "))
+num_dependents = int(input("Enter the number of dependents: "))
+
+taxed_income = gross_income - standard_deduction - \
+                dependent_deduction * num_dependents
+income_tax = taxed_income * tax_rate
+
+print("The income tax is $" + str(income_tax))
+
